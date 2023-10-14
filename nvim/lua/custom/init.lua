136a4c3f0a5e68c2265247ff771a255a31108090
Lukas Leeb <git@lukasl.dev>
2023-10-14 04:10:52 +0200

    nvim: use relative line numbers

---
+++ nvim/lua/custom/init.lua
@@ -0,0 +1,9 @@
+-- Show a column at 80 characters
+vim.opt.colorcolumn = "80"
+
+-- Disable persistent undo
+vim.opt.undofile = false
+
+-- Enable relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
