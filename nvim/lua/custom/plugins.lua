7605f086e2e9e83b0ab748fb98562c214d3c687d
Lukas Leeb <git@lukasl.dev>
2023-10-14 03:46:36 +0200

    nvim: add gopher plugin

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -60,6 +60,17 @@ local plugins = {
       return M
     end,
   },
+
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
 }
 
 return plugins
