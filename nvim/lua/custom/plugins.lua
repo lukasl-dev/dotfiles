c6d72119057cbf5dfd9c46e46892037c3262875b
Lukas Leeb <git@lukasl.dev>
2023-10-14 03:39:56 +0200

    nvim: setup lsp and null-ls for pyright

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,3 +1,5 @@
+local cmp = require("cmp")
+
 local plugins = {
   {
     "zbirenbaum/copilot.lua",
@@ -8,6 +10,55 @@ local plugins = {
     config = function(_, opts)
       require("copilot").setup(opts)
     end
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "pyright",
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+ {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+
+ {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      M.completion.completeopt = "menu,menuone,noselect"
+      M.mapping["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = false,
+      }
+
+      M.mapping["<C-j>"] = cmp.mapping(function(_)
+        cmp.mapping.abort()
+        require("copilot.suggestion").accept_line()
+      end, {
+          "i",
+          "s",
+        })
+
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
   },
 }
 
