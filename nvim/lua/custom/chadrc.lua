7b76a3dd1b29fb40d9937b023a85261cd85b3b11
Lukas Leeb <git@lukasl.dev>
2023-10-14 03:35:04 +0200

    nvim: add copilot

---
+++ nvim/lua/custom/chadrc.lua
@@ -0,0 +1,6 @@
+local M = {}
+
+M.ui = { theme = "catppuccin" }
+M.plugins = "custom.plugins"
+
+return M
---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,14 @@
+local plugins = {
+  {
+    "zbirenbaum/copilot.lua",
+    lazy = false,
+    opts = function ()
+      return require "custom.configs.copilot"
+    end,
+    config = function(_, opts)
+      require("copilot").setup(opts)
+    end
+  },
+}
+
+return plugins
