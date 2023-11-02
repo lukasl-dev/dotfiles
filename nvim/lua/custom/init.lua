424851f3b646ac56311b05ddd4d9364fd217e5f6
Lukas Leeb <git@lukasl.dev>
2023-11-02 13:35:35 +0100

    nvim: use powershell as shell on windows

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -11,3 +11,10 @@ vim.opt.relativenumber = true
 -- Use system clipboard
 vim.opt.clipboard = "unnamedplus"
 
+-- Use Powershell as default shell on Windows
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+  vim.o.shell = 'powershell'
+  vim.o.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command'
+  vim.o.shellquote = '\"'
+  vim.o.shellxquote = ''
+end
