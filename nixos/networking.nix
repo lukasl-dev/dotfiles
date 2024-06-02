37dfa479be2bf1c94cc65ba2c8577c05d0ed81c5
Lukas Leeb <git@lukasl.dev>
2024-06-02 13:51:34 +0200

    nix: use 1.1.1.1 as nameserver

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -6,4 +6,6 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   networking.networkmanager.enable = true;
+
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 }
